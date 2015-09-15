a55a0cc4f0344fb5747f215ba8a765d2e4ea2ce9
Charles Vernon Atkins III <chuck.atkins@kitware.com>
2015-09-15 09:10:09 -0500

    Fix native static build to avoid shared components

--- Projects/png.cmake
+++ Projects/png.cmake
@@ -1,8 +1,14 @@
+if(BUILD_SHARED_LIBS)
+  set(png_shared_args -DPNG_SHARED:BOOL=ON -DPNG_STATIC:BOOL=OFF)
+else()
+  set(png_shared_args -DPNG_SHARED:BOOL=OFF -DPNG_STATIC:BOOL=ON)
+endif()
+
 add_external_project_or_use_system(png
   DEPENDS zlib
 
   CMAKE_ARGS
-    -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+    ${png_shared_args}
     -DPNG_TESTS:BOOL=OFF
     # VTK uses API that gets hidden when PNG_NO_STDIO is TRUE (default).
     -DPNG_NO_STDIO:BOOL=OFF
